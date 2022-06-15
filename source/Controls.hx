32a1fdb6ed6f872e1446f04c8dd5d21cb6488ddc
B side yt <100112873+567bsideyt@users.noreply.github.com>
2022-06-15 17:41:21 +0700

    Update Controls.hx
    Shit

--- source/Controls.hx
+++ source/Controls.hx
@@ -777,4 +777,6 @@ class Controls extends FlxActionSet
 	{
 		return input.device == GAMEPAD && (deviceID == FlxInputDeviceID.ALL || input.deviceID == deviceID);
 	}
-}
+}add(virtualpad)A.B
+(android)
+#end
